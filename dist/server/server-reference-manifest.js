3efd28dd6883ca39347643f0e20f902f9e32155b
Emmanuel Ezekiel <emamex44@gmail.com>
2025-04-19 19:03:40 +0100

    netlify

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qyKVco9ceZ1dA1ylEx99M8f5B/qmdwgFi/QTO05BjDk=\"\n}"
